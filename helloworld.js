3c9485e496580d9016e0e70bbe3e284dbfd32a1d
root <root@EXTPC107.EXT.PAYGATE.NET>
2020-02-13 17:54:15 +0900

    helloworld msg changed

--- helloworld.js
+++ helloworld.js
@@ -54,6 +54,8 @@ let logger = new winston.createLogger({
   exitOnError: false, 
 });
 
+logger.info('Gr Test Server Running');
+
 http.createServer(function (request, response) {
 
    // Send the HTTP header
@@ -62,8 +64,5 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Hello World\n')
+   response.end('Welcome To The Hello World\n')
 }).listen(3000)
-
-// Console will print the message
-logger.info('Gr Test Server running')
